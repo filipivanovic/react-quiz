a6bd5b42eb477fc1f0917630c82e554129e5a3d2
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-02 23:07:26 +0200

    Refactor DateCounter: simplify syntax and apply consistent formatting

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,46 +1,40 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1)
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
+  const date = new Date('june 21 2027')
+  date.setDate(date.getDate() + count)
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
-  };
+    setCount(count => count - step)
+  }
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
-  };
+    setCount(count => count + step)
+  }
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
-  };
+    setCount(Number(e.target.value))
+  }
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
-  };
+    setStep(Number(e.target.value))
+  }
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
-  };
+    setCount(0)
+    setStep(1)
+  }
 
   return (
     <div className="counter">
       <div>
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={step}
-          onChange={defineStep}
-        />
+        <input type="range" min="0" max="10" value={step} onChange={defineStep} />
         <span>{step}</span>
       </div>
 
@@ -56,6 +50,6 @@ function DateCounter() {
         <button onClick={reset}>Reset</button>
       </div>
     </div>
-  );
+  )
 }
-export default DateCounter;
+export default DateCounter
