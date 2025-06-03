ceadd644aa1554b3411d8f06cbfd36b8adf7b12f
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-03 17:22:25 +0200

    Refactor DateCounter: expand useReducer state management with step functionality

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,16 +1,39 @@
-import { useReducer, useState } from 'react'
+import { useReducer } from 'react'
 
 const reducer = (state, action) => {
   console.log(state, action)
-  if (action.type === 'inc') return state + 1
-  if (action.type === 'dec') return state - 1
-  if (action.type === 'setCount') return action.payload
+
+  switch (action.type) {
+    case 'dec':
+      return {
+        ...state,
+        count: state.count - state.step
+      }
+    case 'inc':
+      return {
+        ...state,
+        count: state.count + state.step
+      }
+    case 'setCount':
+      return {
+        ...state,
+        count: action.payload
+      }
+    case 'setStep':
+      return {
+        ...state,
+        step: action.payload
+      }
+    default:
+      throw new Error(`Unknown action type: ${action.type}`)
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0)
+  const initialState = { count: 0, step: 1 }
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const [step, setStep] = useState(1)
+  const { count, step } = state
 
   // This mutates the date object.
   const date = new Date('june 21 2027')
@@ -18,29 +41,21 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: 'dec' })
-    // setCount((count) => count - 1);
-    // setCount(count => count - step)
   }
 
   const inc = function () {
     dispatch({ type: 'inc' })
-    // setCount((count) => count + 1);
-    // setCount(count => count + step)
   }
 
   const defineCount = function (e) {
     dispatch({ type: 'setCount', payload: Number(e.target.value) })
-    // setCount(Number(e.target.value))
   }
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value))
+    dispatch({ type: 'setStep', payload: Number(e.target.value) })
   }
 
-  const reset = function () {
-    // setCount(0)
-    setStep(1)
-  }
+  const reset = function () {}
 
   return (
     <div className="counter">
