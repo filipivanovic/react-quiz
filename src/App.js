505580f2747e4974689edb6f4b16563536a80d13
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-03 21:33:55 +0200

    Integrate useReducer and useEffect in App component for managing questions and fetch status

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import Header from './Header'
 import Main from './Main'
+import { useEffect, useReducer } from 'react'
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+  currentQuestion: 0,
+  score: 0,
+  error: null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready'
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        error: action.payload,
+        status: 'error'
+      }
+    default:
+      throw new Error(`Unknown action type: ${action.type}`)
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    const getQuestion = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/questions')
+        const data = await res.json()
+        dispatch({ type: 'dataReceived', payload: data })
+      } catch (error) {
+        console.error(`Error in getQuestion method: ${error.message || error}`)
+        dispatch({ type: 'dataFailed' })
+      }
+    }
+    getQuestion()
+  }, [])
+
   return (
     <div className="app">
       <Header />
