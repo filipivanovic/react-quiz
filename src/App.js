012d5ba8a73dea930d8ca9c791c2cff0929b2abc
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-03 21:06:27 +0200

    Add Header and Main components to App with basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Header from './Header'
+import Main from './Main'
+
 const App = () => {
-  return <div></div>
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  )
 }
 
 export default App
