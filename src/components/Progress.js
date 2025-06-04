62100ede188168aeb0450affca8a562e8c374252
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-04 22:44:52 +0200

    Display current points and max points in Progress component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,8 +1,11 @@
-const Progress = ({ index, numOfQuestions }) => {
+const Progress = ({ index, numOfQuestions, points, maxPoints }) => {
   return (
     <header className="progress">
       <p>
         Question <strong>{index + 1}</strong> / {numOfQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPoints}
       </p>
     </header>
   )
