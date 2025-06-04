61df60027e74e146ecfb09510f50eaf81c626e67
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-04 23:09:47 +0200

    Update NextButton to show Finish button on the last question and dispatch finish action

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -7,5 +7,12 @@ const NextButton = ({ dispatch, answer, index, numOfQuestions }) => {
         Next
       </button>
     )
+
+  if (index === numOfQuestions - 1)
+    return (
+      <button className="btn btn-ui" onClick={() => dispatch({ type: 'finish' })}>
+        Finish
+      </button>
+    )
 }
 export default NextButton
